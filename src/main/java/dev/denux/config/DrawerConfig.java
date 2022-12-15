4fc2b12fdcfb10a0befe988337e41b25a014e077
-Denux <83671398+DenuxPlays@users.noreply.github.com>
2022-12-15 08:16:36 +0100

    Update DrawerConfig.java

--- src/main/java/dev/denux/config/DrawerConfig.java
+++ src/main/java/dev/denux/config/DrawerConfig.java
@@ -12,14 +12,6 @@ public class DrawerConfig {
     private boolean debugMode = false;
 
     /**
-     * Gets you the debug mode.
-     * @return true if the debug mode is enabled.
-     */
-    public boolean isDebugMode() {
-        return debugMode;
-    }
-
-    /**
      * Updates the debug mode.
      * @param debugMode True if you want to enable it, otherwise false.
      */
